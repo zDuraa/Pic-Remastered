18ec4125551a28d56b20701e83f025e49cb6874e
kai <k.penazzi@web.de>
2024-02-19 22:33:32 +0100

    fix checkDC, add SUBLW + test

---
+++ app/src/main/java/Model/Commands/SUBLW.java
@@ -0,0 +1,24 @@
+package Model.Commands;
+
+import Model.*;
+
+public class SUBLW extends Command {
+    public SUBLW(Pic pic) {
+        super(0b111100, pic);
+    }
+
+    public void execute(int command) {
+        int l = (command & 0b11111111);
+        int ret = pic.w - l;
+
+        checkC(ret);
+        checkZ(ret);
+        checkDC(l, pic.w, '-');
+
+        if (ret < 0) {
+            ret *= -1;
+        }
+
+        pic.w = ret & 0b11111111;
+    }
+}
