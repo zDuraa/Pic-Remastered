f28d5fff256cf329ec7e78fc8b56289578af6af5
kai <k.penazzi@web.de>
2024-02-20 12:28:09 +0100

    added some more tests for the commands missing: decfsz, incfsz, rlf, rrf, nop

--- app/src/test/java/Model/Commands/CommandsTest.java
+++ app/src/test/java/Model/Commands/CommandsTest.java
@@ -73,8 +73,121 @@ public class CommandsTest {
         cmd.execute(20);
         cmd.execute(148);
 
-        assertEquals(0b101010, pic.w);
-        assertEquals(0b101010, pic.ram.getReg(20));
+        assertEquals(0b11101010, pic.w);
+        assertEquals(0b11101010, pic.ram.getReg(20));
+    }
+
+    @Test
+    public void decfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new DECF(pic);
+
+        cmd.execute(20);
+        int z = (pic.ram.getReg(3) & 0b100) >> 2;
+
+        assertEquals(255, pic.w);
+        assertEquals(1, z);
+    }
+
+    public void decfsz() {
+    }
+
+    @Test
+    public void incfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new INCF(pic);
+
+        pic.ram.setReg(20, 255);
+        cmd.execute(20);
+        int z = (pic.ram.getReg(3) & 0b100) >> 2;
+
+        assertEquals(0, pic.w);
+        assertEquals(1, z);
+    }
+
+    @Test
+    public void iorwfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new IORWF(pic);
+
+        pic.w = 0b1001;
+        pic.ram.setReg(29, 0b0101);
+        cmd.execute(29);
+
+        assertEquals(0b1101, pic.w);
+
+        pic.w = 0b0;
+        pic.ram.setReg(12, 0);
+        cmd.execute(12);
+        int z = (pic.ram.getReg(3) & 0b100) >> 2;
+
+        assertEquals(0, pic.w);
+        assertEquals(1, z);
+    }
+
+    @Test
+    public void movfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new MOVF(pic);
+
+        pic.w = 20;
+        cmd.execute(45);
+        int z = (pic.ram.getReg(3) & 0b100) >> 2;
+
+        assertEquals(0, pic.w);
+        assertEquals(z, 1);
+
+    }
+
+    @Test
+    public void movewfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new MOVWF(pic);
+
+        pic.w = 0;
+        pic.ram.setReg(65, 20);
+        cmd.execute(65);
+        int z = (pic.ram.getReg(3) & 0b100) >> 2;
+
+        assertEquals(0, pic.ram.getReg(65));
+        assertEquals(1, z);
+    }
+
+    @Test
+    public void subwfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new SUBWF(pic);
+
+        pic.w = 20;
+        pic.ram.setReg(72, 30);
+        cmd.execute(72);
+        int c = pic.ram.getReg(3) & 0b1;
+
+        assertEquals(10, pic.w);
+        assertEquals(1, c);
+    }
+
+    @Test
+    public void xorwfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new XORWF(pic);
+
+        pic.w = 0b10101;
+        pic.ram.setReg(15, 0b01011);
+        cmd.execute(15);
+
+        assertEquals(0b11110, pic.w);
+    }
+
+    @Test
+    public void swapfTest() {
+        Pic pic = new Pic(new ArrayList<>());
+        Command cmd = new SWAPF(pic);
+
+        pic.ram.setReg(30, 0b00111100);
+        cmd.execute(30);
+
+        assertEquals(0b11000011, pic.w);
     }
 
 }
