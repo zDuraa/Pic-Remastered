7217375ca2875765a4363368908b1b3b2767b8c8
sduramaz <sduramaz@hs-offenburg.de>
2024-02-22 23:25:23 +0100

    Grobes Gerüst von Test7 aufgestellt

---
+++ app/src/test/java/Model/PicTest7.java
@@ -0,0 +1,84 @@
+package Model;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import utils.*;
+
+public class PicTest7 {
+    @Test
+    public void Test7() {
+        FileManager.ladeDatei("./../Testprogramme/TPicSim7.LST");
+        ArrayList<String> commands = FileManager.getCommands();
+
+        Pic pic = new Pic(commands);
+
+        // loop
+        pic.next();//movlw
+        pic.next();//bsf
+        pic.next();//movwf
+        pic.next();//bcf
+        pic.next();//movlw
+        pic.next();//movwf
+        pic.next();//clrf
+
+
+        // loop1
+        for (int i = 0; i < 16; i++) {
+            pic.next();//NOP
+            pic.next();//NOP
+            pic.next();//NOP
+            pic.next();//incf
+            pic.next();//movf
+            pic.next();//btfss
+
+
+            pic.next(); // goto loop1
+        }
+        assertEquals(128, pic.ram.getReg(16)); //num muss 80H in 10h stehen
+
+        pic.next(); //movlw
+        pic.next(); //bsf
+        pic.next(); //movwf
+        pic.next(); //bcf
+        pic.next(); //movlw
+        pic.next(); //movwf
+        pic.next(); //clrf
+
+        // loop2
+        for (int i = 0; i < 16; i++) {
+            pic.next();//incf
+            pic.next();//movf
+            pic.next();//btfss
+            pic.next();//goto loop2
+        }
+        assertEquals(49, pic.ram.getReg(16)); //num muss 31H in 10h stehen
+
+        pic.next(); //movlw
+        pic.next(); //bsf
+        pic.next(); //movwf
+        pic.next(); //bcf
+        pic.next(); //clrf
+
+        // loop3
+        for (int i = 0; i < 16; i++) {
+            pic.next();//btfss
+
+            pic.next();//goto loop3
+        }
+
+        pic.next();//movlw
+        pic.next();//bsf
+        pic.next();//movwf
+        pic.next();//bcf
+        pic.next();//clrf
+
+        // loop4
+        for (int i = 0; i < 16; i++) {
+            pic.next();//btfss
+
+            pic.next();//goto loop4
+        }
+
+    }
+}
