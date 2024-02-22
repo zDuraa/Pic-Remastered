711db20c61919b2c099df6db2adf374dcfd3e0df
kai <k.penazzi@web.de>
2024-02-22 17:40:39 +0100

    fix tetlw

--- app/src/main/java/Model/Commands/RETLW.java
+++ app/src/main/java/Model/Commands/RETLW.java
@@ -9,16 +9,17 @@ public class RETLW extends Command {
         super(0b110100, pic);
     }
 
-    //The W register is loaded with the eight
-    //bit literal ’k’. The program counter is
-    //loaded from the top of the stack (the
-    //return address). This is a two cycle
-    //instruction.
+    // The W register is loaded with the eight
+    // bit literal ’k’. The program counter is
+    // loaded from the top of the stack (the
+    // return address). This is a two cycle
+    // instruction.
     @Override
     public void execute(int command) {
         int l = (command & 0b11111111);
         pic.w = l;
 
         pic.pCounter.set(pic.stack.pop());
+        pic.pCounter.dec();
     }
 }
