5e5bff58d54d490bbbadff813acdfc31c37c03e8
sduramaz <sduramaz@hs-offenburg.de>
2024-02-25 17:09:29 +0100

    GUI changes

---
+++ app/src/main/java/GUI/guiManager.java
@@ -0,0 +1,94 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+
+public class guiManager {
+
+    @FXML
+    private Button buttonGo;
+
+    @FXML
+    private Button buttonIgnore;
+
+    @FXML
+    private Button buttonReset;
+
+    @FXML
+    private Button buttonStepIn;
+
+    @FXML
+    private Button buttonStepOut;
+
+    @FXML
+    private Button buttonStepOver;
+
+    @FXML
+    private ListView<?> listViewCode;
+
+    @FXML
+    private Pane paneINTCONRegister;
+
+    @FXML
+    private Pane paneOptionRegister;
+
+    @FXML
+    private Pane paneRA;
+
+    @FXML
+    private Pane paneRB;
+
+    @FXML
+    private Pane paneRam;
+
+    @FXML
+    private Pane paneSFRSichtbar;
+
+    @FXML
+    private Pane paneSFRversteckt;
+
+    @FXML
+    private Pane paneStack;
+
+    @FXML
+    private Pane paneStatusRegister;
+
+    @FXML
+    private ScrollPane scrollPaneCode;
+
+    @FXML
+    void buttonGoOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonIgnoreOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonResetOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonStepInOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonStepOutOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonStepOverOnClick(ActionEvent event) {
+
+    }
+
+}
+
