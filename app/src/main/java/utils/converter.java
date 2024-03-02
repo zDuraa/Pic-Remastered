1ebee53acefb91459fb72065fc9d5a961ca51a1b
kai <k.penazzi@web.de>
2024-03-02 16:53:49 +0100

    add double 00 in to hex converter

--- app/src/main/java/utils/converter.java
+++ app/src/main/java/utils/converter.java
@@ -2,8 +2,14 @@ package utils;
 
 public class converter {
 
-    public static String intToHex(int iVal){
-        return Integer.toHexString(iVal).toUpperCase();
+    public static String intToHex(int iVal) {
+        String ret = Integer.toHexString(iVal).toUpperCase();
+
+        if (ret.length() == 1) {
+            ret = "0" + ret;
+        }
+
+        return ret;
     }
 
 }
